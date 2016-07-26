c49be1cfb34313cda74e9d7319ce6c962a5c075a
cschmitz <schmitz.chris@gmail.com>
2016-07-26 09:00:58 -0500

    Initial commit of website including changes made during last night's hack night

---
+++ FullStackWebsite/app/Http/Controllers/PeopleController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Person;
+use Illuminate\Http\Request;
+
+class PeopleController extends Controller
+{
+    private $person;
+
+    public function __construct(Person $person)
+    {
+        $this->person = $person;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->person->all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'email|unique:people',
+        ]);
+
+        $newPerson = $this->person->create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+        ]);
+
+        return ['success' => true, 'message' => 'Person saved.', 'data' => ['id' => $newPerson->id]];
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $myperson = $this->person->find($id);
+        return ['success' => true, 'message' => "here's your person", 'data' => [$myperson]];
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,43 @@
+# STL Full Stack Web Development Website
+
+**In Progress**
+
+This is the website for the [Saint Louis Full Stack Web Development](https://www.meetup.com/SaintLouis_FullStack_WebDevelopment/events/231969178/) meetup group. 
+
+This project was started and will be mostly built out during the monthly hack nights. 
+
+At the moment, the plan is to build this website using:
+
+- Laravel
+- MySQL (though we may switch to postgres)
+- Vue.js
+
+## Required tools
+
+To work on this project, you'll need the following tools installed on your computer:
+
+- [PHP (at least version 5.5)](http://php.net/manual/en/install.php)
+- [Composer](https://getcomposer.org/)
+- [Node](https://nodejs.org/en/)
+- [Git](https://git-scm.com/)
+
+You'll also need some kind of web server. Since this is a laravel project I would suggest either:
+
+- [Laravel Valet](https://laravel.com/docs/5.2/valet)
+    - This is a simpler setup and does not involve setting up a virtual machine
+- [Laravel Homestead](https://laravel.com/docs/5.2/homestead) 
+
+Either of these options will be a relativly easy setup. You could also use any other webserver or virual host you'd like to host the project. If you want to check out or work on the site quickly you can even turn on php's built in server via the commands:
+
+    cd path/to/FullStackWebsite/public
+    php -S localhost:8000
+    open http://localhost:8000
+
+## Getting up and running
+
+Once you have the required tools installed, pull down the project from github with the following commands:
+
+    cd path/to/location/you/want/to/storetheprojectin/
+    git clone https://github.com/chris-schmitz/fullstack-website.git fullstack-website
+
+Once cloned, you just need to make sure your webserver is configured to use the `FullStackWebsite/public` directory as it's document root.
